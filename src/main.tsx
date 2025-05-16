1a35b61e91bac6a4cfb381f6b4e359f08a7e56cc
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2025-05-16 06:17:56 +0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
 
 const queryClient = new QueryClient();
 
